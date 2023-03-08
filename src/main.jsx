ea93624dc5f7298cc5a482b994ad83e71e95af3d
Ibrahimhussien227 <ibrahimhussien227@gmail.com>
2023-03-09 01:27:56 +0300

    Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import { store } from "./app/store";
+import { ContextProvider } from "./contexts/ContextProvider";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </Provider>
+  </React.StrictMode>
+);
